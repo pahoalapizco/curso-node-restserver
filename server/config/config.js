c22dca382f840832a7114c81a781014a02374074
Paola Alapizco <paolac.alapizco@gmail.com>
2019-04-21 01:54:50 -0600

    Agregamos variabe de entorno para ocular la cadenade conexion a la BD

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // BASE DE DATOS
 // =======================
 
-let DB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://pahoalapizco:btdkVlkg1U6NTsbu@cafecluster-lb65t.mongodb.net/cafe'
+let DB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.BD_URI
 
 process.env.DB = DB
