e79d1a07c254266b77920bad59bd58efbcd1777b
Paola Alapizco <paolac.alapizco@gmail.com>
2019-05-04 00:36:56 -0600

    Se conclyen CRUD de productos

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -5,7 +5,7 @@ const {
   PAGINA_INICIO_DEFAULT,
   MAX_PAGINA_DEFAULT
 } = require('../config/globals')
-
+const _ = require('underscore')
 const app = express()
 
 // Obtener lista de todos los productos
@@ -27,7 +27,7 @@ app.get('/producto', verificaToken, (req, res) => {
         })
       }
       if(!produtosDB){
-        return res.status(500).json({
+        return res.status(400).json({
           ok: false,
           err:{
             message: 'No hay productos registrados.'
@@ -41,27 +41,74 @@ app.get('/producto', verificaToken, (req, res) => {
     })
 })
 
-// ===========
-// TODO
-// ===========
 // Obtener un producto en especifico
 app.get('/producto/:id', verificaToken, (req, res) => {
   // agregar populate: usuario y categoria
+  let _id = req.params.id
+
+  Producto.findById({ _id })
+    .populate('categoria', 'descripcion')
+    .populate('usuario', 'nombre email')
+    .exec((err, productoBD) => {
+      if(err){
+        return res.status(500).json({
+          ok: false,
+          err
+        })
+      }
+      if(!productoBD){
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: 'Producto no encontrado.'
+          }
+        })
+      }
+      res.json({
+        ok: true,
+        producto: productoBD
+      })
+    })
 
 })
 
+app.get('/producto/buscar/:termino', (req, res) => {
+  let termino = new RegExp( req.params.termino, 'i' )
+
+  Producto.find({nombre: termino})
+    .populate('categoria', 'nombre')
+    .exec((err, productosBD) =>{
+      if(err){
+        return res.status(500).json({
+          ok: false,
+          err
+        })
+      }
+      if(!productosBD || productosBD.length == 0){
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: 'Producto no encontrado.'
+          }
+        })
+      }
+      res.json({
+        ok: true,
+        productos: productosBD
+      })
+    })
+})
 // Registrar nuevo producto 
 app.post('/producto', verificaToken, (req, res) => {
   let body = req.body
   let usuario = req.usuario
   let producto =  new Producto ({
     nombre: body.nombre,
-    precioUni: body.precio,
+    precioUni: body.precioUni,
     descripcion: body.descripcion,
     categoria: body.categoria,
-    usuario//: usuario.id
+    usuario: usuario._id
   })
-
   producto.save((err, productoDB) => {
     if(err){
       return res.status(500).json({
@@ -70,9 +117,11 @@ app.post('/producto', verificaToken, (req, res) => {
       })
     }
     if(!productoDB){
-      return res.status(500).json({
+      return res.status(400).json({
         ok: false,
-        err
+        err: {
+          message: 'Lo sentimos, ell producto no se ha registrado.'
+        }
       })
     }
     res.json({
@@ -82,20 +131,59 @@ app.post('/producto', verificaToken, (req, res) => {
   })
 })
 
-// ===========
-// TODO
-// ===========
 // Actualizar un producto en especfico.
 app.put('/producto/:id', verificaToken, (req, res) => {
+  let id = req.params.id
+  let productoBody = req.body
+
+  Producto.findByIdAndUpdate(id, productoBody, {new: true, runValidators: true},(err, productoBD) => {
+      if(err){
+        return res.status(500).json({
+          ok: true,
+          err
+        })
+      }
+      if(!productoBD){
+        return res.status(400).json({
+            ok: false,
+          err: {
+            message: 'Producto no encontrado'
+          }
+        })
+      }
+      res.json({
+        ok: true,
+        producto: productoBD
+      })
+    })
 
 })
 
-// ===========
-// TODO
-// ===========
 // Elimina un producto en especifico
 app.delete('/producto/:id', [verificaToken, verificaAdminRol], (req, res) => {
   // cambiar el valor del key disponible a false
+  let id = req.params.id
+  
+  Producto.findByIdAndUpdate(id, { disponible: false }, {new: true}, (err, productoBD) => {
+    if(err){
+      return res.status(500).json({
+        ok: false,
+        err
+      })
+    }
+    if(!productoBD){
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Producto no encontrado'
+        }
+      })
+    }
+    res.json({
+      ok: true,
+      producto: productoBD
+    })
+  })
 })
 
 module.exports = app
